0e841110ad7a96e67082c6ca6b18a6fafaa374f3
Fan Yang <fan.yang.turner@live.co.uk>
2019-10-02 17:03:54 +0100

    Add generate mask nf and script

---
+++ bin/genRefArray.py
@@ -0,0 +1,70 @@
+import sys
+import argparse
+
+def load_reference(reflines):
+    reference = [x.strip() for x in reflines if x and x[0] not in ['>']]
+    reference = ''.join(reference)
+    return reference
+
+def load_regions(regionlines):
+    regions = [x.split('\t')[1:-1] for x in regionlines if x and x[0] not in ['>', '#']]
+    regions = [(int(x[0]), int(x[1])) for x in regions]
+    return regions
+
+def generate_array(regions, reflen):
+    mask = list('0' * reflen)
+
+    for low, high in regions:
+        for x in range(low, high + 1):
+            mask[x - 1] = '1'
+
+    mask = ''.join(mask)
+    return mask
+
+def intersperse(data, linelen, sep='\n'):
+    ret = list()
+
+    for x in range(0, (len(data) // linelen) + 1):
+        ret.append(data[x*linelen:(x+1)*linelen] + sep)
+    return ''.join(ret)
+
+print('-')
+print(intersperse('00000000000000000000000000000000', 10))
+print('-')
+def main():
+    '''
+    Generate mask array file based on repetitive region (bed file), in the following format:
+    # number from 1, coordinates inclusive of start and stop
+    NC_000962.3 5   10	1
+    NC_000962.3	80184	80373	1
+
+    Input:
+        -r reference file
+        -g region bed file
+    
+    Output:
+        masked array file (0 for nomask, 1 for mask)
+
+    Run:
+        python3 bin/genRefArray.py -g NC_000962_3.fasta.rpt.regions -r ref.fa
+    '''
+    parser = argparse.ArgumentParser(description='Generate mask array')
+
+    parser.add_argument('-r', '--reference', dest='reference', help='path to the reference file')
+    parser.add_argument('-g', '--regions', dest='regions', help='masked region from blast')
+
+    args = parser.parse_args()
+
+    regionlines = open(args.regions).readlines()
+    regions = load_regions(regionlines)
+       
+    reflines = open(args.reference).readlines()
+    reference = load_reference(reflines)
+
+    mask = generate_array(regions, len(reference))
+
+    intersperse(mask, 100, '\n')
+
+
+if __name__ == "__main__":
+    main()
---
+++ genRefMask.nf
@@ -0,0 +1,57 @@
+#!/usr/bin/env nextflow
+/* 
+Mask a reference (fasta file) using script bin/genRefMask.py
+nextflow run genRefMask.nf --refFile /data/references/compass/fasta/NC_000962_3.fasta --outputPath mask_75_90
+*/
+
+
+// parameters 
+params.outputPath = "example_output"
+params.refFile = "example_data/R00000039.fasta"
+
+// initial logging
+log.info "\n" 
+log.info "BUGflow -- version 0.1"
+log.info "Input reference file   :  ${params.refFile}"
+log.info "Output path            :  ${params.outputPath}"
+log.info "Container engine       :  ${workflow.containerEngine}"
+log.info "Container              :  ${workflow.container}"
+log.info "Profile                :  ${workflow.profile}"
+log.info "\n"
+
+
+// rename input parameters
+refFasta = file(params.refFile)
+outputPath = file(params.outputPath)
+
+// Build indexes for reference fasta file - bwa, samtools, repeatmask
+process indexReference {
+  
+    input:
+        file refFasta
+	
+	output:
+		file "*" into ref_index
+	
+	tag {refFasta}
+	publishDir "$outputPath/reference/${refFasta.baseName}", mode: 'copy'
+
+    """
+    #bwa index
+    bwa index $refFasta
+    
+    #samtools index
+    samtools faidx $refFasta
+    
+    #blast indexes for self-self blast
+	makeblastdb -dbtype nucl -in $refFasta
+    
+    #reference mask
+    genRefMask.py -r $refFasta -m 75 -p 90
+    bgzip -c ${refFasta}.rpt.regions > ${refFasta.baseName}.rpt_mask.gz
+	echo '##INFO=<ID=RPT,Number=1,Type=Integer,Description="Flag for variant in repetitive region">' > ${refFasta.baseName}.rpt_mask.hdr
+	tabix -s1 -b2 -e3 ${refFasta.baseName}.rpt_mask.gz
+    
+    """
+}
+
